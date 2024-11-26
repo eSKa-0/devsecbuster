47290016e6c1502b98e15461b8e7ea7af542eae9
eSKa-0 <166024351+eSKa-0@users.noreply.github.com>
2024-11-26 13:14:59 +0100

    Add files via upload

--- devsecbuster.py
+++ devsecbuster.py
@@ -1,22 +1,36 @@
 import requests
 import json
 import getpass
-from dependencies import banner
 import os
+import argparse
+
+parser = argparse.ArgumentParser(prog="devsecbuster", description="used to find sometimes hidden directories.")
+parser.add_argument("-t", "--target", required=True, type=str, help="The target ip adress")
+parser.add_argument("-w", "--wordlist", required=True, type=str, help="The wordlist used")
+parser.add_argument("-p", "--protocol", help="Use if the website is http", action="store_true")
+args = parser.parse_args()
+
+print(args.target)
 
 bool = True
 header = {
     "Content-Type": "application/json",
-    "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML>
+    "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML>"
 }
 positivelistdenied = []
 positivelistacces = []
-
 if bool == True:
-    protocol = input("what protocol does the url use? ")
-    url = input("What url do you want to test? ")
-    wordlist =  input("Wordlist to use? ")
-    r = open(f"{wordlist}")
+    if args.protocol:
+        protocol = "http"
+    else:
+        protocol = "https"
+    url = args.target
+    wordlist =  args.wordlist
+    try:    
+        r = open(f"{wordlist}")
+    except:
+        print("File not found or invalid.")
+        bool = False
     buffer = r.read()
     buffer = buffer.split("\n")
     for i in buffer:
@@ -35,23 +49,3 @@ if bool == True:
     for i in positivelistacces:
         print(f"\t\t{i}")
     bool = False
-if input("Try again? ") in ["y", "yes"]:
-    bool = True
-
-        c = requests.get(urm, headers=header)
-        print(f"{urm}\t\t{c.status_code}")
-        if c.status_code == 403:
-            positivelistdenied.append(f"{urm}\t\t{c.status_code}")
-        elif c.status_code == 200:
-            positivelistacces.append(f"{urm}\t\t{c.status_code}")
-    print("Usable:")
-    print("\tunaccesible:")
-    for i in positivelistdenied:
-        print(f"\t\t{i}")
-    print("\taccesable:")
-    for i in positivelistacces:
-        print(f"\t\t{i}")
-    bool = False
-if input("Try again? ") in ["y", "yes"]:
-    bool = True
-
